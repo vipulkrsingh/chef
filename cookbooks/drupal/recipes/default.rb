3855ba0095aca136784dad6e68e1a4e5f33f6143
Vipul Kumar Singh <vipul@entrayn.com>
2016-06-10 19:24:10 +0530

    add apt packages

--- cookbooks/drupal/recipes/default.rb
+++ cookbooks/drupal/recipes/default.rb
@@ -12,8 +12,8 @@ end
 
 include_recipe 'apt'
 
-package ['apache*','php*'] do
+package ['apache*','php*','mysql*','node-less','nodejs','npm'] do
   action :remove
 end
 
-package ['php5-cli']
+package ['nginx','php5-fpm','php5-mysql','mysql-server-5.5','php5-gd','php5-curl','php5-cli','php5-dev','php5-mcrypt','imagemagick','aptitude','memcached','libmemcached-tools','php-pear','make']
